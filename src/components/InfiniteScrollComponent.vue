29a0258885221ef4b048e4e012d47395c7b8eccf
gshohat <gilad.shohat@gmail.com>
2023-09-13 10:53:58 +0200

    component@add

---
+++ src/components/InfiniteScrollComponent.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+const props = defineProps(['options']);
+const items = await props.options.fetchData()
+
+</script>
+
+
+<template>
+
+  <div class="scrollbar">
+    <slot :items="items"></slot>
+  </div>
+
+</template>
+
+<style scoped>
+
+.scrollbar {
+  overflow: scroll;
+  height: 90%;
+}
+
+</style>
+
